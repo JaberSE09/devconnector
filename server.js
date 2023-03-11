af9421788eddece227e90a5f5ef96cbbaa97a5f7
Sajid Jaber <sajidjaber@yahoo.com>
2023-03-11 13:00:49 -0600

    user model setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require("express")
-const connectDB = require("./config/db")
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-connectDB()
-app.get("/", (req,res) => res.send("API Running") )
+connectDB();
+app.get("/", (req, res) => res.send("API Running"));
 
-const PORT = process.env.PORT || 5000
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
 
-app.listen(PORT , () => console.log(`server on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server on port ${PORT}`));
