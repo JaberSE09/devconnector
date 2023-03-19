e883c8f9c0fe11f82b5db6b1132f0e00d985364e
Sajid Jaber <sajidjaber@yahoo.com>
2023-03-19 15:46:13 -0500

    get profile route setup

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,6 +14,6 @@ module.exports = function (req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: "token is  valid" });
+    res.status(401).json({ msg: "token is not valid" });
   }
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require("express");
 
 const router = express.Router();
+const auth = require("../../middleware/auth")
+const Profile = require("../../models/Profile")
+const User = require("../../models/User")
+//get a single profile
+router.get("/me", auth ,async (req, res) => {
 
-router.get("/", (req, res) => {
-  res.send("Profile Route");
+try {
+    
+    const profile = await Profile.findOne({user: req.user.id}).populate("user", ["name" , "avatar"] )
+
+    if(!profile){
+        return res.status(400).json({msg: "there is no profile for this user"})
+    }
+
+    res.json(profile)
+} catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error")
+}
+  
+
+
+
 });
 
 module.exports = router;
